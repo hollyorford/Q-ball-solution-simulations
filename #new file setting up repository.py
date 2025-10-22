668032c183c38454c39e9f9d462d41a03bb473c2
hollyorford <h.orford@lancaster.ac.uk>
2025-10-22 19:45:20 +0100

    Working DHO

--- #new file setting up repository.py
+++ #new file setting up repository.py
@@ -3,35 +3,55 @@
 #Damped Harmonic Oscillator Practice
 
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 # Basic Equation y.. + 2 gamma y. = -omega^2 y
 # rewrite as a pair of coupled first order equations y. = z and y..=z.
 
-y=0
+y=10
 dy=0
 z=0
 dz=0
 t=0
-dt=0.1
-gamma = 0.1 #float(input('What is your damping factor?'))
+dt=0.001
+gamma = 0.01 #float(input('What is your damping factor?'))
 omega = 1
 y_list = []
 t_list = []
-
-while t >= 500:
+x_line = []
+b = 0
+print('Data produced at times 8s, 25s, 5s, 78s')
+while t <= 100:
     dy=z*dt
-    dz=((-2*gamma*z)-((omega^2)*y))*dt
+    dz=((-2*gamma*z)-((omega**2)*y))*dt
     z=z+dz
+    if b == 8000:
+        print('Time is ' + str(t) + 's')
+        print('Time interval ' + str(dt) + 's')
+        print('Position is ' + str(y) + 'm' + '\n')
+    if b == 25000:
+        print('Time is ' + str(t))
+        print('Time interval ' + str(dt))
+        print('Position is ' + str(y) + '\n')
+    if b == 52000:
+        print('Time is ' + str(t))
+        print('Time interval ' + str(dt))
+        print('Position is ' + str(y) + '\n')
+    if b == 78000:
+        print('Time is ' + str(t))
+        print('Time interval ' + str(dt))
+        print('Position is ' + str(y) + '\n')
     y=y+dy
     t=t+dt
     y_list.append(y)
     t_list.append(t)
+    
+    x_line.append(0)
+    b = b + 1
 
-
-#plt.title('Path of Proton in Synchrocyclotron', fontsize=16)
-plt.plot(y_list, t_list, color='#008080')
-plt.xlabel('Position',fontsize=16)
-plt.ylabel('Time',fontsize=16)
-#plt.tick_params(axis='both', labelsize=16) 
+plt.plot(t_list, y_list, color='#008080')
+plt.plot(t_list, x_line, color='#000000')
+plt.axvline(x=8, color='red', linestyle='--')
+plt.xlabel('Time /s',fontsize=22)
+plt.ylabel('Position /m',fontsize=22)
 plt.show()
